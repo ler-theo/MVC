46958644f2bd1821f19a9f1820d9ba392ced7179
ler-theo <lerquet.theo@orange.fr>
2017-01-09 14:20:20 +0100

    Tentative de faire des choses

--- index.php
+++ index.php
@@ -5,11 +5,14 @@ class Model
 {
 
   public $string;
+  public $string2;
 
   public function __construct()
   {
 
-    $this -> string = "Page d'acceuil";
+    $this -> string = "Page 1";
+
+    $this -> string2 = "Page 2";
 
   }
 
@@ -27,14 +30,22 @@ class Controller
     $this -> model = $model;
   }
 
-  public function clicked()
+  public function page1()
+  {
+    $this -> model -> string = "Page 2";
+    $this -> model -> string2 = "Acceuil";
+  }
+
+  public function page2()
   {
     $this -> model -> string = "Page 1";
+    $this -> model -> string2 = "Acceuil";
   }
 
   public function acceuil()
   {
-    $this -> model -> string = "Page 2";
+    $this -> model -> string = "Page 1";
+    $this -> model -> string2 = "Page 2";
   }
 
 
@@ -46,7 +57,8 @@ class View
 
   private $model;
   private $controller;
-  public $action = "clicked";
+  public $action = "page1";
+  public $action2 = "page2";
 
   public function __construct($controller, $model)
   {
@@ -56,21 +68,33 @@ class View
 
   public function output()
   {
-    return "<p><a href=\"index.php?action=" . $this -> action . "\">" . $this -> model -> string . "</a></p>";
+    return
+    "<p><a href=\"index.php?action=" . $this -> action . "\">" . $this -> model -> string . "</a></p>"
+     .
+    "<p><a href=\"index.php?action=" . $this -> action2 . "\">" . $this -> model -> string2 . "</a></p>";
   }
-
   public function toggle($action)
   {
-    if ($action === "clicked") {
+    if ($action === "page1") {
 
-      $this -> action = "acceuil";
+      $this -> action = "page2";
+      $this -> action2 = "acceuil";
 
     } elseif ($action === "acceuil") {
 
-      $this -> action = "clicked";
+      $this -> action = "page1";
+      $this -> action2 = "page2";
 
+    } elseif ($action === "page2") {
+
+      $this -> action = "page1";
+      $this -> action2 = "acceuil";
+
+    } elseif (empty($action)) {
+      $this -> action = "acceuil";
     }
   }
+
 }
 
 
@@ -83,15 +107,28 @@ $controller = new Controller($model);
 //Instance 3, injection de 2(1) et de 1
 $view = new View($controller, $model);
 
-$actionGet = $_GET['action'];
-// Condition pour definir l'action realisez
-if (isset($_GET['action']) && !empty($actionGet) && $actionGet === 'clicked') {
+//Verification si le $_GET est definie
+if (isset($_GET['action'])) {
 
+  //Si oui, on le stock dans une variable pour le $_GET
+  $actionGet = $_GET['action'];
+
+}
+
+// Verification de l'action
+if (isset($_GET['action']) && !empty($actionGet) && $actionGet === 'acceuil') {
+
+  //Appel de la method toggle
   $view -> toggle($actionGet);
 
   $controller -> {$actionGet}();
 
-} elseif (isset($_GET['action']) && !empty($actionGet) && $actionGet === 'acceuil') {
+} elseif (isset($_GET['action']) && !empty($actionGet) && $actionGet === 'page1') {
+
+  $view -> toggle($actionGet);
+
+  $controller -> {$actionGet}();
+} elseif (isset($_GET['action']) && !empty($actionGet) && $actionGet === 'page2') {
 
   $view -> toggle($actionGet);
 
